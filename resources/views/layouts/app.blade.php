399d6ee0a6fada90997b65b3ada5bb7db180ad97
palaiTB <ppadarabinda@gmail.com>
2019-11-08 02:12:10 +0530

    https serve

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
